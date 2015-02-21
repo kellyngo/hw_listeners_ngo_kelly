1c5bcd786ab5ae58ef27716b516b05b71d291890
Kelly Ngo <kellyngo726@gmail.com>
2015-02-20 21:45:11 -0500

    add add button event listener

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-var button = document.getElementsByClassName ( "button" );
+var button = document.getElementById ( "button" );
 
-button.addEventListener("click", function() {
-    var newButton = document.createElement ("button");
+button.addEventListener( "click", function() { //listens for the button to be clicked
+    var newButton = document.createElement ("button"); //creates a new button when the button is clicked
     newButton.innerHTML = "Another Button";
 
-    var main = document.getElementById ("main");
+    var main = document.getElementById ("main"); //adds the new button to the section with id="main"
     main.appendChild (newButton);
 });
