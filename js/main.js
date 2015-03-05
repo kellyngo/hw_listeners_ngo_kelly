bb7e2fc1838e4d899adf0a58e004e4967d0eea4c
Richard Kalehoff <richardkalehoff@gmail.com>
2015-03-05 01:22:41 -0500

    remove unneeded add function

--- js/main.js
+++ js/main.js
@@ -2,14 +2,10 @@ var myHeading = document.getElementById ("myHeading");
 
 var counter = 1;
 
-function add () {
-     return counter += 1;
-}
-
 myHeading.addEventListener( "click", function() {
     var newGraf = document.createElement ("p");
     newGraf.innerHTML = "This is click " + counter;
-    add();
+    counter += 1;
 
     var wrapper = document.getElementById ("wrapper");
     wrapper.appendChild (newGraf);
